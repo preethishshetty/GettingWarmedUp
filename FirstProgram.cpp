0d18bd7bb7dd2dc2b1b3af8351174d14b63da604
Preethish Shetty <preethish.gunner@gmail.com>
2015-09-01 15:20:35 -0700

    Made changes on the Master Branch.

--- FirstProgram.cpp
+++ FirstProgram.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main()
-{
-	cout<<"Hello Moto!";
+{   
+    cout<<"Hello Moto!";
     cout<<"Okay. Friend.";
+    cout<<"Tweaking and tinkering with GitHub Version Control";
 }
